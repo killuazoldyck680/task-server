ba032284d895add0af00b6d89c187890a1fc7bd1
killuazoldyck680 <killuaz2049@gmail.com>
2026-06-05 22:18:05 +0530

    feat: implement baseline task data structure and serialize json endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(home_page));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("Server running up at http://{}", addr);
+
+    let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
+
+    axum::serve(listener, app).await.unwrap();
+
 }
+async fn home_page() -> &'static str {
+    "Hello! You have successfully reached your brand-new Rust server!"
+}
+
