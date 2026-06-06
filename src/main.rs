dad98948a64872f35529bd7eb195be5e11fc648d
killuazoldyck680 <killuaz2049@gmail.com>
2026-06-06 19:38:53 +0530

    feat: implement baseline task data structure and serialize json endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
-use axum::{routing::get, Router};
+use axum::{routing::get, Router, Json};
 use std::net::SocketAddr;
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct Task {
+    id: u64,
+    title: String,
+    completed: bool,
+}
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(home_page));
+    let app = Router::new().route("/", get(home_page))
+    .route("/task", get(get_task));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     println!("Server running up at http://{}", addr);
@@ -15,5 +24,15 @@ async fn main() {
 }
 async fn home_page() -> &'static str {
     "Hello! You have successfully reached your brand-new Rust server!"
+}
+
+async fn get_task() -> Json<Task> {
+    let my_todo = Task {
+        id: 1,
+        title: String::from("Learn Rust Web Development"),
+        completed: false,
+    };
+
+    Json(my_todo)
 }
 
