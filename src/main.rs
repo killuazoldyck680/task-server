7caf1f11b405045e512b28943ad1628c77471dad
killuazoldyck680 <killuaz2049@gmail.com>
2026-06-07 20:06:19 +0530

    feat: upgrade routing syntax to support dynamic path parameters for update and delete actions

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use axum::{Json, Router, extract::State, routing::{get, post}};
+use axum::http::StatusCode;
+use axum::{Json, Router, extract::{State,Path}, routing::{get, post,patch,delete}};
 use std::net::SocketAddr;
 use serde::{Serialize,Deserialize};
 use std::sync::{Arc, Mutex};
+
 
 #[derive(Serialize, Clone)]
 struct Task {
@@ -25,6 +27,9 @@ async fn main() {
     let app = Router::new().route("/", get(home_page))
     .route("/task", get(get_all_task))
     .route("/task", post(create_task))
+    
+    .route("/task/{id}", patch(toggle_task_completion))
+    .route("/task/{id}", delete(delete_task))
     .with_state(shared_state);
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
@@ -62,6 +67,32 @@ async fn create_task  (
 
     Json(new_task)
 }
+
+async fn toggle_task_completion(State(state): State<AppState>,
+    Path(id): Path<u64>) -> Result<Json<Task>, StatusCode> {
+        let mut tasks = state.lock().unwrap();
+
+        if let Some(task) = tasks.iter_mut().find(|t| t.id == id) {
+            task.completed = !task.completed;
+            Ok(Json(task.clone()))
+        } else {
+            Err(StatusCode::NOT_FOUND)
+        }
+    }
+
+async fn delete_task(State(state): State<AppState>, Path(id): Path<u64>) -> Result<String, StatusCode> {
+    let mut tasks = state.lock().unwrap();
+
+    let initial_len = tasks.len();
+
+    tasks.retain(|task| task.id != id);
+
+    if tasks.len() < initial_len {
+       Ok(format!("Task {} was successfully deleted", id)) 
+    } else {
+        Err(StatusCode::NOT_FOUND)
+    }
+}    
 
 
 
